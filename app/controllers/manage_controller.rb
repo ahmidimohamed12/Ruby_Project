eba0004e1c2346e0df002aa8b3a588ba94bb45f3
ahmidimohamed12 <ahmidinador123@gmail.com>
2020-02-24 16:03:21 +0100

    add

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,12 +1,8 @@
 class ManageController < ApplicationController
   def index
-    @fruit = ["banana","appel"]
     render("index.html.erb")
   end
  def home
-    #render("index.html.erb")
-  end
-  #def list
-  #  render('index2.html.erb')
-  #end
+  render("home.html.erb");
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
  get 'manage/index'
+ get 'manage/home'
 #get 'manage/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  # root "manage#index"
